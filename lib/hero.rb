c15161f594751f0a447f94a589d48ba4ec3f479e
ryuuryuu <fran7ryuuryuu@icloud.com>
2019-10-18 10:49:20 +0900

    1018 課題

--- lib/hero.rb
+++ lib/hero.rb
@@ -1,1 +1,17 @@
 # 勇者 (Hero) のふるまいを表すクラス
+
+class Hero
+  attr_reader :name, :hp
+
+  def initialize(name, hp)
+    # @name = Yanbarukuina
+    # @hp = 13
+    # @atk = 3
+    # @def = 2
+    @name = name
+    @hp = hp
+  end
+  def dead?
+    @hp <= 0
+  end
+end
--- test/hero_test.rb
+++ test/hero_test.rb
@@ -5,13 +5,34 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new]
 
-# テスト対象のRubyソースコードを読み込む
+# # テスト対象のRubyソースコードを読み込む
 require_relative '../lib/hero.rb'
 
-# テストを実行するためのクラス
+# # テストを実行するためのクラス
 class HeroTest < Minitest::Test
   # Heroクラスのインスタンスが生成(new)できることを確認する
-  def test_hero
-    assert Hero.new
+  # def test_hero
+  #   assert Hero.new
+  # end
+  # テストを実行する前の準備
+  def setup
+    @hero = Hero.new('ああああ', 50)
+  end
+
+  def test_name
+    assert_equal(@hero.name, 'ああああ')
+  end
+
+  def test_hp
+    assert_equal(@hero.hp, 50)
+  end
+  def test_dead
+    hero_hp1 = Hero.new('ああああ', 1)
+    hero_hp0 = Hero.new('ああああ', 0)
+    hero_hpminus = Hero.new('ああああ', -1)
+
+    assert_equal(hero_hp1.dead?, false)
+    assert_equal(hero_hp0.dead?, true)
+    assert_equal(hero_hpminus.dead?, true)
   end
 end
